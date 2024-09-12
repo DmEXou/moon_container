a3a707c1572007c7b4c72bac2c81215714a9a543
DmEXou <91079520+DmEXou@users.noreply.github.com>
2024-09-13 00:04:38 +0700

    add test imput and test output files.

--- program.cpp
+++ program.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 int main() {
+    std::string s;
+    std::ifstream file("input.txt");
 
+    if (!file) {
+        std::cout << "file opening failed!\n";
+        return EXIT_FAILURE;
+    }
+    while (std::getline(file, s)) {
+        std::cout << s << std::endl;
+    }
 
-
+    file.close();
 	return 0;
 }
